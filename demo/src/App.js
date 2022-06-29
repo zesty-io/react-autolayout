5964cb35aa24f15d868cfeffc7ad9a50c04a949f
Gian Espinosa <espinosagian1995@gmail.com>
2022-06-30 01:10:58 +0800

    changes to AutoLayout logic and add content also as props

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,19 +1,22 @@
-import "./App.css";
+import './App.css'
 
-import { AutoLayout } from "@zesty-io/react-autolayout";
-
+import { AutoLayout } from '@zesty-io/react-autolayout'
 import { CustomTextarea } from './CustomTextarea'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AutoLayout url={'https://4q6k83l9-dev.webengine.zesty.io/zop-gun/?toJSON'} components={{
-          "textarea": CustomTextarea
-        }} />
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                {/* You can pass url or content object */}
+                <AutoLayout
+                    url={'https://4q6k83l9-dev.webengine.zesty.io/zop-gun/?toJSON'}
+                    components={{
+                        textarea: CustomTextarea
+                    }}
+                />
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
